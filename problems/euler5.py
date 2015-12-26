a9f674c30a7c04eccea41b186bddadfa218b8491
jayman386 <jmoriarty@gatech.edu>
2015-12-25 22:32:52 -0600

    Second commit

--- problems/euler5.py
+++ problems/euler5.py
@@ -5,22 +5,14 @@
 What is the smallest positive number that is evenly divisible by all of the
 numbers from 1 to 20?
 '''
+top = 2000
+bucket = 1
 
-result = 0
-start = 1
-end = 1000
-my_list = []
-
-def divisible(n):
-    divisible = True
-    for i in range(11,21):
-        if n % i != 0:
-            divisible = False
-
-    return divisible
-
-while len(my_list) == 0:
-    my_list = [x for x in range(start,end) if divisible(x)]
-    start = end
-    end = end * 2
-print my_list
+for i in range(1,1 + top):
+    currentBucket = bucket
+    multiple = 1
+    print i - 1, bucket
+    while not currentBucket % i == 0:
+        currentBucket = bucket * multiple
+        multiple += 1
+    bucket = currentBucket
